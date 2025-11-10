a2caa4e1aa4b05c36f4bba6d7e05d40a8d5b1ba3
annamani <annamanik.vit@gmail.com>
2025-11-10 13:57:41 +0100

    assignment file seperated  week-wise

---
+++ courses/foundation/intro-to-javascript/Week-2/flight-booking.js
@@ -0,0 +1,8 @@
+function getFullName(fullName1, fullName2) {
+    console.log(fullName1, fullName2);
+    // console.log('"' + fullName1, fullName2 + '"');
+}
+const firstName = "Annamani";
+const lastName = "Kamma";
+getFullName(firstName, lastName);
+getFullName("Benjamin", "Hughes"); // returns "Benjamin Hughes"
---
+++ courses/foundation/intro-to-javascript/Week-2/formal-fullname.js
@@ -0,0 +1,13 @@
+function getFullName(fullName1, fullName2, useFormalName) {
+    if (useFormalName) {
+        console.log("Lord ", fullName1, fullName2);
+    }
+    else {
+        console.log(fullName1, fullName2);
+    }
+}
+const firstName = "Annamani";
+const lastName = "Kamma";
+getFullName(firstName, lastName, true);// returns "Lord Annamani Kamma"
+getFullName("Benjamin", "Hughes", false); // returns "Benjamin Hughes"
+getFullName("Jemimag", "Rodgarus", true); 

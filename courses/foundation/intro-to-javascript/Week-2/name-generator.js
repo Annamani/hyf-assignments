607cf10447ff21d6b5ef2d1371f75c84787999c2
annamani <annamanik.vit@gmail.com>
2025-11-11 12:45:17 +0100

    move file to week-1

--- courses/foundation/intro-to-javascript/Week-2/name-generator.js
+++
@@ -1,7 +0,0 @@
-const firstWords = ['Easy', 'Awesome', 'Corporate', 'Innovative', 'Dynamic', 'Global', 'NextGen', 'Agile', 'Smart', 'Scalable'];
-const secondWords = ['Solutions', 'Technologies', 'Systems', 'Concepts', 'Match', 'Strategies', 'Applications', 'Platforms', 'Networks', 'Services'];
-//create a variable called startupName.
-const startupName = firstWords[Math.floor(Math.random() * firstWords.length)] + " " + secondWords[Math.floor(Math.random() * secondWords.length)];
-console.log(startupName);
-console.log("The startup: " + startupName + " contains" + startupName.length + " characters");
-

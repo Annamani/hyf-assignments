9f01e5fdc30459d1ef2c72cea45c3c45ee440932
annamani <annamanik.vit@gmail.com>
2025-11-11 13:10:04 +0100

    gender logic added in full-name function

--- courses/foundation/intro-to-javascript/Week-2/formal-fullname.js
+++ courses/foundation/intro-to-javascript/Week-2/formal-fullname.js
@@ -1,14 +1,19 @@
-function getFullName(fullName1, fullName2, useFormalName) {
-    if (useFormalName) {
-        console.log("Lord ", fullName1, fullName2);
-    }
-    else {
-        console.log(fullName1, fullName2);
+function getFullName(name, surname, useFormalName, gender) {
+    if (useFormalName || gender) {
+        if (gender === "female") {
+            console.log("Madam ", name, surname)
+        } else if (gender === "male") {
+            console.log("Lord ", name, surname)
+        } else {
+            console.log(name, surname);
+        }
     }
 }
+
 const firstName = "Annamani";
 const lastName = "Kamma";
-getFullName(firstName, lastName, true);// returns "Lord Annamani Kamma"
-getFullName("Benjamin", "Hughes", false); // returns "Benjamin Hughes"
+getFullName(firstName, lastName, true, "female");// returns "Lord Annamani Kamma"
+getFullName("Benjamin", "Hughes", false, "male"); // returns "Benjamin Hughes"
 getFullName("Jemimag", "Rodgarus", true); //returns "Lord  Jemimag Rodgarus"
 getFullName("John", "Minduag");// returns John Minduag
+getFullName();

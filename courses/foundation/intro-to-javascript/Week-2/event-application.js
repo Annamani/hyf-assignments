9196cb67dfbc0409e5728bddd11bfc52ec9c135d
annamani <annamanik.vit@gmail.com>
2025-11-11 01:10:54 +0100

    even-application file updated

--- courses/foundation/intro-to-javascript/Week-2/event-application.js
+++ courses/foundation/intro-to-javascript/Week-2/event-application.js
@@ -1,1 +1,12 @@
-const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+function getEventWeekday(eventDate) {
+    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const todayDate = new Date().getDate();
+    // console.log(todayDate);
+    const eventDay = (todayDate + eventDate) % weekDays.length;
+    return weekDays[eventDay];
+
+}
+
+console.log(getEventWeekday(10));
+console.log(getEventWeekday(2));
+console.log(getEventWeekday(20));

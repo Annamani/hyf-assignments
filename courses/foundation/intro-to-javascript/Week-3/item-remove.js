26951d7b8d4e2137c25bffff748f10db787785db
annamani <annamanik.vit@gmail.com>
2025-11-12 13:15:02 +0100

    item remove file created

---
+++ courses/foundation/intro-to-javascript/Week-3/item-remove.js
@@ -0,0 +1,13 @@
+const names = [
+    "Peter",
+    "Ahmad",
+    "Yana",
+    "kristina",
+    "Rasmus",
+    "Samuel",
+    "Katrine",
+    "Tala",
+];
+const nameToRemove = "Ahmad";
+
+console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'Katrine', 'Tala']

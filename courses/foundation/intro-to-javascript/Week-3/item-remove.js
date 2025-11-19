80125b0e2dfb78842129f5c0fdb65310c59419a5
Kamma Annamani <annamanik.vit@gmail.com>
2025-11-19 11:05:56 +0100

    Refactor item removal to use filter method
    Replaced the removal logic with a filter method.

--- courses/foundation/intro-to-javascript/Week-3/item-remove.js
+++ courses/foundation/intro-to-javascript/Week-3/item-remove.js
@@ -10,21 +10,5 @@ const names = [
 ];
 console.log("Initital Array: ", names)
 const nameToRemove = "Ahmad";
-// for (let i = 0; i < names.length; i++) {
-//     if (names[i] === nameToRemove) {
-//         // splice
-//         names.splice(i, 1)
-//         break
-
-//     }
-// }
-
-
-// const index = names.indexOf(nameToRemove);
-// if (index !== -1) {
-//     names.splice(index, 1);
-// }
-//console.log("Modified Array: ", names);
-
 const modifiedArray = names.filter(name => name !== nameToRemove);
 console.log("Modified Array: ", modifiedArray); 

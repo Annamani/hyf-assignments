4b88873fbf74a8850c24b050c9d3c3588d73e49e
annamani <annamanik.vit@gmail.com>
2025-11-24 12:08:49 +0100

    max and min elements of an array

---
+++ courses/foundation/intro-to-javascript/Week-4/high_and_low.js
@@ -0,0 +1,24 @@
+function highAndLow(numbers) {
+    const numArray = numbers.split(' ');
+    for (let i = 0; i < numArray.length; i++) {
+        numArray[i] = parseInt(numArray[i]);
+    }
+    let maxNum = numArray[0];
+    let minNum = numArray[0];
+    if (numArray.length === 0) {
+        console.log("Please provide a valid input");
+    } else {
+        for (let i = 1; i < numArray.length; i++) {
+            if (numArray[i] > maxNum) {
+                maxNum = numArray[i];
+            }
+            if (numArray[i] < minNum) {
+                minNum = numArray[i];
+            }
+        }
+        console.log(maxNum + " " + minNum);
+    }
+}
+highAndLow("1 2 3 4 5"); // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"

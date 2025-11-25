96ae676a27f279cf5fced48621c49cdd2212c2ee
annamani <annamanik.vit@gmail.com>
2025-11-25 14:38:12 +0100

    today details logic updated

--- courses/foundation/intro-to-javascript/Week-4/voice_assistant.js
+++ courses/foundation/intro-to-javascript/Week-4/voice_assistant.js
@@ -1,7 +1,6 @@
 const getReply = (command) => {
     let userName = "";
     let todoList = [];
-    command = command.trim();
     if (command.startsWith("Hello my name is")) {
         const name = command.replace("Hello my name is", "").trim();
         getReply.userName = name;
@@ -10,7 +9,7 @@ const getReply = (command) => {
 
     if (command === "What is my name?") {
         if (!getReply.userName) {
-            return "Sorry, I don't know your name.";
+            return "Sorry, I don't know your name.Please provide your name.";
         }
         else {
             return `Your name is ${getReply.userName}`;
@@ -18,29 +17,80 @@ const getReply = (command) => {
     }
     if (command.startsWith("Add") && command.includes("to my todo")) {
         const task = command.replace("Add", "").replace("to my todo", "").trim();
+        if (!task) {
+            return "Please specify a task to add to your todo.";
+        }
+        todoList = getReply.todoList || [];
         todoList.push(task);
+        getReply.todoList = todoList;
+        // console.log(todoList);
         return `${task} added to your todo`;
     }
     if (command.startsWith("Remove") && command.includes("from my todo")) {
-        const task = command.replace("Remove", "").replace("from my todo", "").trim();
-        const taskIndex = todoList.indexOf(task);
-        if (taskIndex >= 0) {
-            todoList.splice(taskIndex, 1);
-            return `${task} removed from your todo`;
-        } else {
-            return `${task} is not in your todo`;
+        const taskName = command.replace("Remove", "").replace("from my todo", "").trim();
+        todoList = getReply.todoList || [];
+        if (todoList.indexOf(taskName) !== -1) {
+            todoList.splice(todoList.indexOf(taskName), 1);
+            return `${taskName} removed from your todo`;
+        }
+        else {
+            return `${taskName} is not in your todo`;
         }
     }
 
     if (command === "What is on my todo?") {
+        todoList = getReply.todoList || [];
         if (todoList.length >= 1) {
-            return `You have ${todoList.length} tasks on your todo : ${todoList.join(", ")}`;
+            return `You have ${todoList.length} tasks on your todo : ${todoList.join(" , ")}`;
+        } else {
+            return "Your todo list is empty.";
         }
-        // else {
-        //     return "Your todo list is empty.";
-        // }
     }
-}
+    if (command === "What day is it today?") {
+        const today = new Date();
+        const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        return `Today is ${weekDays[today.getDay()]}, ${today.getDate()}. of ${months[today.getMonth()]} ${today.getFullYear()}`;
+    }
+    if (command.startsWith("what is")) {
+        const expression = command.replace("what is", "").trim();
+        // console.log(expression);
+        const operators = ["+", "-", "*", "/"];
+        for (let operator of operators) {
+            if (expression.includes(operator)) {
+                const expressionParts = expression.split(operator).map(part => part.trim());
+                // console.log(expressionParts);
+                if (expressionParts.length === 2) {
+                    const num1 = parseFloat(expressionParts[0]);
+                    const num2 = parseFloat(expressionParts[1]);
+                    let result;
+                    switch (operator) {
+                        case "+":
+                            result = num1 + num2;
+                            break;
+                        case "-":
+                            result = num1 - num2;
+                            break;
+                        case "*":
+                            result = num1 * num2;
+                            break;
+                        case "/":
+                            result = num1 / num2;
+                            break;
+                    }
+                    return `${expression} is ${result}`;
+                }
+            }
+        }
+    }
+
+    if (command.startsWith("Set a timer for") && command.includes("minutes")) {
+        const timePart = command.replace("Set a timer for", "").replace("minutes", "").trim();
+        //When 4 minutes is up: "Timer done".---NEED TO DOOO
+        return `Timer set for ${timePart} minutes`;
+    }
+    return "I'm sorry, I didn't understand that command.";
+};
 console.log(getReply("Hello my name is Benjamin")); // "Nice to meet you benjamin"
 console.log(getReply("What is my name?")); // "Your name is Benjamin"
 console.log(getReply("Add fishing to my todo")); // "fishing added to your todo"
@@ -53,5 +103,7 @@ console.log(getReply("what is 3 + 3")); // "3 + 3 is 6"
 console.log(getReply("what is 4 * 12"));    // "4 * 12 is 48"
 console.log(getReply("Set a timer for 4 minutes")); // "Timer set for 4 minutes"
 console.log(getReply("Add reading a book to my todo")); // "reading a book added to your todo"
-console.log(getReply("Add going for a walk to my todo"));
-console.log(getReply("Remove check all bills from my todo"));
+console.log(getReply("Add going for a walk to my todo"));   // "going for a walk added to your todo"
+console.log(getReply("Remove check all bills from my todo")); // "check all bills is not in your todo"
+console.log(getReply("What is on my todo?"));   // "You have 4 tasks on your todo: singing in the shower , cleaning the house , reading a book , going for a walk"\
+console.log(getReply("How is the weather today?")); // "I'm sorry, I didn't understand that command."

c0e9a0c521d6562244f32c54bf87311b1631f8c0
annamani <annamanik.vit@gmail.com>
2025-12-15 14:02:11 +0100

    new routes comments added

--- courses/foundation/intro-to-backend/exercise2.js
+++ courses/foundation/intro-to-backend/exercise2.js
@@ -8,7 +8,7 @@ const port = 3000;
 const knexInstance = knex({
   client: "sqlite3",
   connection: {
-    filename: "C:\\Users\\annam\\Documents\\Database\\tasks.sqlite3",
+    filename: "C:\\Users\\annam\\Documents\\Database\\database.sqlite3",
   },
   useNullAsDefault: true,  // Omit warning in console
 });
@@ -17,13 +17,31 @@ app.get("/", (req, res) => {
   res.send("Hello from exercise 2!");
 });
 
-// Here is an example of the first route, /all-users, which returns all users sorted by their ID
+//all-users, which returns all users sorted by their ID
 app.get("/all-users", async (req, res) => {
-  const rows = await knexInstance.raw("SELECT * FROM user ORDER BY id ASC;");
+  const rows = await knexInstance.raw("SELECT * FROM users ORDER BY id ASC;");
   res.json(rows);
 });
+///unconfirmed-users should respond with unconfirmed users
 
-// TODO implement more routes here
+//gmail-users should respond with users with an @gmail.com email
+app.get("/gmail-users", async (req, res) => {
+  const rows = await knexInstance.raw("SELECT * FROM users WHERE email LIKE '%@gmail.com'");
+  res.json(rows);
+});
+//2022-users should respond with users created in 2022
+
+//user-count should respond with the number of users
+app.get("/user-count", async (req, res) => {
+  const result = await knexInstance.raw("select count(*)as total_users from users");
+  res.json(result);
+});
+// last-name-count should respond with how many users there are with a given last name, sorted alphabetically
+
+//first-user should respond with the first user. If there are no users in the table, respond with a 404
+
+//Update the / route to return a HTML page that fetches the count value 
+// from the /user-count route you've implemented previously.
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);

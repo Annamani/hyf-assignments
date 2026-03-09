d4e50449451faba62dd37fe518ebefbda7da408a
Annamani <annamanik.vit@gmail.com>
2026-03-09 12:39:01 +0100

    Rewrite for-loop code using Array methods filter and map

---
+++ courses/backend/advanced-javascript/Week-1/exercise1.js
@@ -0,0 +1,6 @@
+import { teas as data } from "../data/teas.js";
+// Exercise 1: Rewrite with Array Methods
+const result = data
+  .filter((tea) => tea.caffeineLevel !== "none")
+  .map((tea) => tea.name.toUpperCase())
+console.log(result);

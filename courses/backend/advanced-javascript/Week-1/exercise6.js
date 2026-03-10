bddab0542dd8fc6188910464b523541ba36071ef
Annamani <annamanik.vit@gmail.com>
2026-03-10 12:01:34 +0100

    inventory Value exercise updated using reduce method

---
+++ courses/backend/advanced-javascript/Week-1/exercise6.js
@@ -0,0 +1,7 @@
+// Exercise 6: Total Inventory Value (Optional)
+import { teas as data } from "../data/teas.js";
+const totalValue = data.reduce((sum, tea) => 
+  sum+tea.pricePerGram * tea.stockCount
+, 0);
+
+console.log("Total inventory value:", totalValue);

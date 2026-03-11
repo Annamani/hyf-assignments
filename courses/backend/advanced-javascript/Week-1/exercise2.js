3934135487708556cff434dc2fd46a7e00f5ad86
Annamani <annamanik.vit@gmail.com>
2026-03-11 11:39:27 +0100

    exercise4 logic changed using only forEach loop

--- courses/backend/advanced-javascript/Week-1/exercise2.js
+++ courses/backend/advanced-javascript/Week-1/exercise2.js
@@ -1,18 +1,18 @@
 import { teas as data } from "../data/teas.js";
 // Exercise 2: Inventory Report ⭐
 function inventoryReport(teas) {
-  let inventoryValue=0;
-  let totalTeaPrice=0;
-  teas.forEach((tea)=>{
-    inventoryValue+=tea.pricePerGram * tea.stockCount;
-    totalTeaPrice+=tea.pricePerGram;
+  let inventoryValue = 0;
+  let totalTeaPrice = 0;
+  teas.forEach((tea) => {
+    inventoryValue += tea.pricePerGram * tea.stockCount;
+    totalTeaPrice += tea.pricePerGram;
   });
   return {
     totalTeas: teas.length,
     inStock: teas.filter((tea) => tea.inStock).length,
     outOfStock: teas.filter((tea) => !tea.inStock).length,
-    totalInventoryValue:inventoryValue,
-    averagePrice:totalTeaPrice/ teas.length,
+    totalInventoryValue: inventoryValue,
+    averagePrice: totalTeaPrice / teas.length,
   };
 }
 console.log(inventoryReport(data));
--- courses/backend/advanced-javascript/Week-1/exercise4.js
+++ courses/backend/advanced-javascript/Week-1/exercise4.js
@@ -1,16 +1,11 @@
 import { teas as data } from "../data/teas.js";
 // Exercise 4: Teas by Origin ⭐⭐
 function teasByOrigin(teas) {
-  // Return object where keys are origins and values are arrays of tea names
-  const teaOrigins = [...new Set(teas.map((tea) => tea.origin))];
   const result = {};
-  teaOrigins.map((origin) => {
-    result[origin] = teas
-      .filter((tea) => tea.origin === origin)
-      .map((tea) => tea.name);
+  teas.forEach((teaElement) => {
+    if (!result[teaElement.origin]) result[teaElement.origin] = [];
+    result[teaElement.origin].push(teaElement.name);
   });
   return result;
 }
-
 console.log(teasByOrigin(data));
-

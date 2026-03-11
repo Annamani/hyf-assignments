8699ec1be7ef91a461e808f30b4f96e3ed49da3b
Annamani <annamanik.vit@gmail.com>
2026-03-11 11:04:50 +0100

    corrected minor styling issue

--- courses/backend/advanced-javascript/Week-1/exercise1.js
+++ courses/backend/advanced-javascript/Week-1/exercise1.js
@@ -2,5 +2,5 @@ import { teas as data } from "../data/teas.js";
 // Exercise 1: Rewrite with Array Methods
 const result = data
   .filter((tea) => tea.caffeineLevel !== "none")
-  .map((tea) => tea.name.toUpperCase())
+  .map((tea) => tea.name.toUpperCase());
 console.log(result);

743adc35fa3cae2193d9d341dbfb074af0dd6f81
Annamani <annamanik.vit@gmail.com>
2026-03-17 15:27:59 +0100

    feat:Order Processing System updated with CheclStock logic

--- courses/backend/advanced-javascript/Week-2/exercise2.js
+++ courses/backend/advanced-javascript/Week-2/exercise2.js
@@ -11,8 +11,8 @@ const order = {
   ],
 };
 //validateOrder(order, callback);
-function validateOrder(order, callback){
- setTimeout(() => {
+function validateOrder(order, callback) {
+  setTimeout(() => {
     const errors = [];
     order.items.forEach((item) => {
       const exists = data.some((tea) => tea.id === item.teaId);
@@ -20,35 +20,65 @@ function validateOrder(order, callback){
         errors.push(`Tea with id ${item.teaId} does not exist`);
       }
     });
-    const result = {
+    const validateOrderResult = {
       valid: errors.length,
       errors: errors,
     };
-    callback(result);
+    callback(validateOrderResult);
   }, 200);
 }
 // calculateTotal(order, callback);
-function calculateTotal(order, callback){
- setTimeout(() => {
-   let totalPrice = 0;
-   order.items.forEach((item) => {
-     const tea = data.find((tea) => tea.id === item.teaId);
-     if (tea) {
-       totalPrice += tea.pricePerGram * item.grams;
-     }
-   });
-   const result = {
-     orderId: order.id,
-     total: totalPrice,
-   };
+function calculateTotal(order, callback) {
+  setTimeout(() => {
+    let totalPrice = 0;
+    order.items.forEach((item) => {
+      const tea = data.find((tea) => tea.id === item.teaId);
+      if (tea) {
+        totalPrice += tea.pricePerGram * item.grams;
+      }
+    });
+    const calculateTotalResult = {
+      orderId: order.id,
+      total: totalPrice,
+    };
 
-   callback(result);
- }, 300);
+    callback(calculateTotalResult);
+  }, 300);
+}
+function checkStock(order, callback) {
+  setTimeout(() => {
+    const shortages = [];
+    let inStock = true;
+    order.items.forEach((item) => {
+      const tea = data.find((t) => t.id === item.teaId);
+      if (!tea) {
+        shortages.push(`Tea ID ${item.teaId} not found in inventory`);
+        inStock = false;
+      } else if (!tea.inStock) {
+        shortages.push(`${tea.name} is out of stock`);
+        inStock = false;
+      } else if (tea.stockCount < item.grams) {
+        shortages.push(
+          `${tea.name} - Requested ${item.grams} grams, but only ${tea.stockCount} grams in stock`,
+        );
+        inStock = false;
+      }
+    });
+    const checkStockResult = {
+      orderId: order.id,
+      inStock: inStock,
+      shortages: shortages,
+    };
+    callback(checkStockResult);
+  }, 400);
 }
 validateOrder(order, (result) => {
-  console.log("Validation result:", result);
+  console.log("validateOrder result:", result);
 });
 
 calculateTotal(order, (result) => {
-  console.log("Validation result:", result);
+  console.log("calculateTotal result:", result);
+});
+checkStock(order, (result) => {
+  console.log("checkStock result:", result);
 });
